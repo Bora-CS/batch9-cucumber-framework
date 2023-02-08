682b87135afd585d8be23f62729139653d30d9f9
Inhae Jeong <inhae.j@gmail.com>
2023-02-08 17:36:32 -0500

    uploadFunction

--- src/test/java/utilities/Util.java
+++ src/test/java/utilities/Util.java
@@ -1,16 +1,25 @@
 package utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class Util {
 
 	/*
 	 * Sleeps for given number of seconds
 	 */
-	public static void wait(int seconds) {
-		try {
-			Thread.sleep(seconds * 1000);
-		} catch (Exception e) {
-			e.printStackTrace();
+	public static void uplad (WebDriver driver, String uploadElement) {		
+			/*
+			 * upload file
+			 * @Author: Inhae Jeong
+			 * 
+			 */
+				WebElement upload = driver.findElement(By.xpath(uploadElement));
+				upload.sendKeys("C:/Users/Desktop/upload.png");
+				System.out.println("File is Uploaded Successfully");
+			
 		}
 	}
 
-}
+
