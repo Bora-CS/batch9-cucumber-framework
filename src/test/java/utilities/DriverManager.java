13f553ba6ace86da4994b5cb3b067c59f5d42cd6
SedefAkyol <sedefakyol12@gmail.com>
2023-02-08 16:54:24 -0600

    firt commit

--- src/test/java/utilities/DriverManager.java
+++ src/test/java/utilities/DriverManager.java
@@ -1,5 +1,8 @@
 package utilities;
 
+import java.util.Properties;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,18 +12,26 @@ public class DriverManager {
 	public static WebDriver driver;
 
 	public static void main(String[] args) {
+		
+		Properties sProps = System.getProperties();
+		
+		Set<Object> keys = sProps.keySet();
 
-		try {
-			System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver_mac_m1");
-			driver = new ChromeDriver();
-			driver.get("https://boratech-practice-app.onrender.com/");
-			driver.findElement(By.xpath("//*[contains(text(), 'Login')]")).click();
-			Thread.sleep(3000);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			driver.quit();
+		for (Object key : keys) {
+			System.out.println("Key: " + key + " - Value: " + sProps.get(key));
 		}
+
+//		try {
+//			System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver_mac_m1");
+//			driver = new ChromeDriver();
+//			driver.get("https://boratech-practice-app.onrender.com/");
+//			driver.findElement(By.xpath("//*[contains(text(), 'Login')]")).click();
+//			Thread.sleep(3000);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			driver.quit();
+//		}
 
 	}
 
