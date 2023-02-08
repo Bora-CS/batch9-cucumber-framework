941a2612bf99e016639da149039456e9c65ed8b4
Jack Vu <jacknvu98@gmail.com>
2023-02-08 12:11:51 -0500

    add button click function

--- src/test/java/utilities/Util.java
+++ src/test/java/utilities/Util.java
@@ -1,6 +1,19 @@
 package utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class Util {
+
+	
+	/*
+	 * Clicks on buttons based on xpath of button 
+	 * @Author: Jack Vu 
+	 * 
+	 */
+	public static void buttonClickFunction(WebDriver driver, String xpathOfButton) {
+		driver.findElement(By.xpath(xpathOfButton)).click();
+	}
 
 	/*
 	 * Sleeps for given number of seconds
