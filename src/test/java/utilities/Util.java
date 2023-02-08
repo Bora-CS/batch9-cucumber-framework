63e8e895248dbf46b9b510ec4ac7780ade4d0f9f
Surayya Ebirayim <surayyaebirayim@Surayyas-Air.attlocal.net>
2023-02-08 11:12:43 -0800

    updat Actions

--- src/test/java/utilities/Util.java
+++ src/test/java/utilities/Util.java
@@ -1,7 +1,22 @@
 package utilities;
 
-public class Util {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
+public class Util {
+	
+    /*
+     * @Author: surayya Abraham
+     * 
+     */
+	public static void dragAndDropAcions(WebDriver driver, String sourceXpath , String targetXpath) {
+		Actions action = new Actions(driver);
+		WebElement drage = driver.findElement(By.xpath("sourceXpath"));
+		WebElement drop = driver.findElement(By.xpath("targetXpath"));
+		 action.dragAndDrop(drage, drop).perform();
+	}
 	/*
 	 * Sleeps for given number of seconds
 	 */
