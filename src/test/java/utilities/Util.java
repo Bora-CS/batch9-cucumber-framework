4bcde8853bc334f523a3e83865231d2612e85158
Mustafa Wumaier <mustafawumaier@gmail.com>
2023-02-08 23:45:01 -0500

    moveToElement

--- src/test/java/utilities/Util.java
+++ src/test/java/utilities/Util.java
@@ -1,16 +1,29 @@
 package utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class Util {
 
 	/*
-	 * Sleeps for given number of seconds
+	 * moveToElement
+	 * 
+	 * @Author: Mustafa W
 	 */
-	public static void wait(int seconds) {
-		try {
-			Thread.sleep(seconds * 1000);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public static void  moveToElement() throws InterruptedException {
+		System.setProperty("webdriver.chrome,driver", "src/test/resources/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demoqa.com/droppable");
+		driver.manage().window().maximize();
+		
+		Actions actions = new Actions(driver);
+		
+		WebElement targetLocation = driver.findElement(By.id("toolTipButton"));
+		Thread.sleep(3000);
+		actions.moveToElement(targetLocation).build().perform();
 	}
 
 }
