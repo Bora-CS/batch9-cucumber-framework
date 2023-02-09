103092d6a8054e33ffc328c3c930d5eb184fdced
SedefAkyol <sedefakyol12@gmail.com>
2023-02-08 22:49:54 -0600

    SedefMultiOption

--- src/test/java/utilities/Util.java
+++ src/test/java/utilities/Util.java
@@ -1,7 +1,39 @@
 package utilities;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
 public class Util {
 
+	/*
+	 * 
+	 * select multi option
+	 * @Author: Sedef Akyo
+	 */
+	
+
+	public static void selectMulti(WebDriver driver, String xpathLocator, String optionValue) {
+	Select multiElem = new Select(driver.findElement(By.xpath(xpathLocator)));
+
+	multiElem.selectByValue(optionValue);
+	multiElem.selectByValue(optionValue);
+	
+	List<WebElement> lists = multiElem.getOptions();
+	for(WebElement options:lists) {
+		System.out.println("The options of the dropdown are :"+ options.getText());
+		
+		
+			}
+		}
+		
+	
+		
+	
+	
 	/*
 	 * Sleeps for given number of seconds
 	 * 
