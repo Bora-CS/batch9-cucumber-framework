c06a28303e753e2ce95c31a110bd1ffe8d8427e0
Subhinur Muhammad <subhinurmuhammad@Subhinurs-MBP.fios-router.home>
2023-02-09 10:43:36 -0500

    Adding sendkeys and select functions

--- src/test/java/utilities/Util.java
+++ src/test/java/utilities/Util.java
@@ -1,7 +1,30 @@
 package utilities;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
 
 public class Util {
-
+	/*
+	 * Selecting Drop Down List by invisible text
+	 * @Author: Subhinur Muhammad
+	 */
+	public static void selectByVisibleText(WebElement element, String text) {
+		
+		Select select = new Select(element);
+		select.selectByVisibleText(text);
+		
+	}
+	
+	/*
+	 * Adding string in text field
+	 * @Author: Subhinur Muhammad
+	 */
+	public static void  addText(WebDriver driver, By locator, String value) {
+		WebElement field = driver.findElement(locator); 
+		field.sendKeys(value);
+	}
 	/*
 	 * Sleeps for given number of seconds
 	 * 
