45bb685717933c29c324a593c05e880ee5497ec8
Jolene Jones <jolenejoba@gmail.com>
2023-02-10 14:51:26 -0500

    minor changes

--- src/test/java/utilities/Util.java
+++ src/test/java/utilities/Util.java
@@ -1,5 +1,8 @@
 package utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class Util {
 
 	/*
@@ -14,5 +17,26 @@ public class Util {
 			e.printStackTrace();
 		}
 	}
+
+}
+	/*
+	 * Switches the driver focus between the windows.
+	 * 
+	 * @Author Jolene Jones
+	 */
+
+	public static void switchTo(WebDriver driver, By locater) {
+
+		String parent = driver.getWindowHandle();
+		driver.findElement(locater).click();
+		for (String newwin : driver.getWindowHandles()) {
+			driver.switchTo().window(newwin);
+		}
+		driver.close();
+		driver.switchTo().window(parent);
+
+	}
+
+}
 
 }
