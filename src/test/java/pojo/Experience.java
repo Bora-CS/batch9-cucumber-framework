a9c24268637a4e1c9a4127c914c149182de02853
Erlyn Gammad <gammaderlyn@gmail.com>
2023-02-14 21:35:41 -0800

    adding the Experience class under pojo package

--- src/test/java/pojo/Experience.java
+++ src/test/java/pojo/Experience.java
@@ -1,5 +1,12 @@
 package pojo;
 
 public class Experience {
+	public String company;
+	public String title;
+	public String location;
+	public String from;
+	public String to;
+	public String current;
+	public String description;
 
 }
